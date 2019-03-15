94bce35e9e0a2f6dc29351433fb3c7e165f5ef3f
george <gkephart@cnm.edu>
2019-03-14 23:43:31 -0600

    Apis have been retrofitted.

--- public_html/api/image/index.php
+++ public_html/api/image/index.php
@@ -8,7 +8,7 @@ require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 
 use Edu\Cnm\DataDesign\{
-	Tweet, Image
+	Tweet, Image, Profile
 };
 
 /**
@@ -33,6 +33,7 @@ try {
 	// Grab the MySQL connection
 	$secrets = new \Secrets("/etc/apache2/capstone-mysql/ddctwitter.ini");
 	$pdo = $secrets->getPdoObject();
+	$cloudinary = json_decode($pdo->getSecret("cloudinary"));
 
 	//determine which HTTP method is being used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
@@ -41,8 +42,7 @@ try {
 	$profileId = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-	$config = readConfig("/etc/apache2/capstone-mysql/ddctwitter.ini");
-	$cloudinary = json_decode($config["cloudinary"]);
+
 	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
 
 
@@ -60,47 +60,10 @@ try {
 			$reply->data = Image::getImageByProfileId($pdo, $profileId)->toArray();
 		}
 
-	} elseif($method === "DELETE") {
+	}  elseif($method === "POST") {
 
 		//enforce that the end user has a XSRF token.
 		verifyXsrf();
-
-		// retrieve the Image to be deleted
-		$image = Image::getImageByImageId($pdo, $id);
-		if($image === null) {
-			throw(new RuntimeException("Image does not exist", 404));
-		}
-
-		//enforce the user is signed in and only trying to edit their own image
-		// use the image id to get the tweet id to get the profile id to compare it to the session profile id
-
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== Tweet::getTweetByTweetId($pdo, $image->getImageTweetId())->getTweetProfileId()) {
-			throw(new \InvalidArgumentException("You are not allowed to delete this image", 403));
-		}
-
-		//enforce the end user has a JWT token
-		validateJwtHeader();
-
-		// delete image from cloudinary
-		$cloudinaryResult = \Cloudinary\Uploader::destroy($image->getImageCloudinaryToken());
-		// delete image database
-		$image->delete($pdo);
-		// update reply
-		$reply->message = "Image deleted OK";
-
-	} elseif($method === "POST") {
-
-		//enforce that the end user has a XSRF token.
-		verifyXsrf();
-
-		// verify the user is logged in
-		if(empty($_SESSION["profile"]) === true) {
-			throw (new \InvalidArgumentException("you must be logged in to post images", 401));
-
-			// verify user is logged into the profile posting the tweet before uploading an image
-		} elseif($_SESSION["profile"]->getProfileId() !== Tweet::getTweetByTweetId($pdo, $tweetId)->getTweetProfileId()) {
-			throw(new \InvalidArgumentException("You are not allowed to post an image to someone else's tweet", 403));
-		}
 
 		// assigning variable to the user profile, add image extension
 		$tempUserFileName = $_FILES["image"]["tmp_name"];
--- public_html/api/tweet/index.php
+++ public_html/api/tweet/index.php
@@ -38,10 +38,9 @@ try {
 	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
-	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profile = Profile::getProfileByProfileEmail($pdo, "userman.jane@jones.com");
+
 	$tweetProfileId = filter_input(INPUT_GET, "tweetProfileId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-	$tweetContent = filter_input(INPUT_GET, "tweetContent", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	//make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true )) {
@@ -59,7 +58,7 @@ try {
 			$reply->data = Tweet::getTweetByTweetId($pdo, $id);
 		} else if(empty($tweetProfileId) === false) {
 			// if the user is logged in grab all the tweets by that user based  on who is logged in
-			$reply->data = Tweet::getTweetByTweetProfileId($pdo, $tweetProfileId);
+			$reply->data = Tweet::getTweetByTweetProfileId($pdo, $profile->getProfileId())->toArray();
 		} else if(empty($tweetContent) === false) {
 			$reply->data = Tweet::getTweetByTweetContent($pdo, $tweetContent)->toArray();
 		} else {
@@ -69,10 +68,6 @@ try {
 		// enforce the user has a XSRF token
 		verifyXsrf();
 
-		// enforce the user is signed in
-		if(empty($_SESSION["profile"]) === true) {
-			throw(new \InvalidArgumentException("you must be logged in to post tweets", 401));
-		}
 
 		$requestContent = file_get_contents("php://input");
 		// Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
@@ -82,40 +77,7 @@ try {
 		if(empty($requestObject->tweetContent) === true) {
 			throw(new \InvalidArgumentException ("No content for Tweet.", 405));
 		}
-
-		// make sure tweet date is accurate (optional field)
-		if(empty($requestObject->tweetDate) === true) {
-			$requestObject->tweetDate = null;
-		}
-
-		//perform the actual put or post
-		if($method === "PUT") {
-
-			// retrieve the tweet to update
-			$tweet = Tweet::getTweetByTweetId($pdo, $id);
-			if($tweet === null) {
-				throw(new RuntimeException("Tweet does not exist", 404));
-			}
-
-			//enforce the end user has a JWT token
-
-
-			//enforce the user is signed in and only trying to edit their own tweet
-			if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $tweet->getTweetProfileId()->toString()) {
-				throw(new \InvalidArgumentException("You are not allowed to edit this tweet", 403));
-			}
-
-			validateJwtHeader();
-
-			// update all attributes
-			//$tweet->setTweetDate($requestObject->tweetDate);
-			$tweet->setTweetContent($requestObject->tweetContent);
-			$tweet->update($pdo);
-
-			// update reply
-			$reply->message = "Tweet updated OK";
-
-		} else if($method === "POST") {
+		if($method === "POST") {
 
 			// enforce the user is signed in
 			if(empty($_SESSION["profile"]) === true) {
@@ -126,37 +88,14 @@ try {
 			validateJwtHeader();
 
 			// create new tweet and insert into the database
-			$tweet = new Tweet(generateUuidV4(), $_SESSION["profile"]->getProfileId(), $requestObject->tweetContent, null);
+			$tweet = new Tweet(generateUuidV4(), $profile->getProfileId(), $requestObject->tweetContent, null);
 			$tweet->insert($pdo);
 
 			// update reply
 			$reply->message = "Tweet created OK";
 		}
 
-	} else if($method === "DELETE") {
-
-		//enforce that the end user has a XSRF token.
-		verifyXsrf();
-
-		// retrieve the Tweet to be deleted
-		$tweet = Tweet::getTweetByTweetId($pdo, $id);
-		if($tweet === null) {
-			throw(new RuntimeException("Tweet does not exist", 404));
-		}
-
-		//enforce the user is signed in and only trying to edit their own tweet
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $tweet->getTweetProfileId()->toString()) {
-			throw(new \InvalidArgumentException("You are not allowed to delete this tweet", 403));
-		}
-
-		//enforce the end user has a JWT token
-		validateJwtHeader();
-
-		// delete tweet
-		$tweet->delete($pdo);
-		// update reply
-		$reply->message = "Tweet deleted OK";
-	} else {
+	}  else {
 		throw (new InvalidArgumentException("Invalid HTTP method request", 418));
 	}
 // update the $reply->status $reply->message
