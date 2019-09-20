34b68f06b11dea99c82d5d8e8cb7d2dff272f77d
george <gkephart@cnm.edu>
2019-09-20 12:40:57 -0600

    added the store to the home page

--- app/src/index.js
+++ app/src/index.js
@@ -7,18 +7,31 @@ import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {Profile} from "./pages/Profile";
+import {applyMiddleware, createStore} from "redux";
+import {combinedReducers} from "./shared/reducers/reducers";
+import thunk from "redux-thunk";
+import Provider from "react-redux/lib/components/Provider";
 
 library.add();
 
-const Routing = () => (
+
+const store = createStore(combinedReducers, applyMiddleware(thunk));
+
+library.add();
+
+const Routing = (store) => (
 	<>
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/profile" component={Profile} />
-				<Route exact path="/" component={Home}/>
-				<Route component={FourOhFour}/>
-			</Switch>
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/profile/" component={Profile}/>
+					<Route exact path="/" component={Home}/>
+					<Route component={FourOhFour}/>
+				</Switch>
+			</BrowserRouter>
+		</Provider>
 	</>
 );
-ReactDOM.render(<Routing/>, document.querySelector('#root'));
+
+ReactDOM.render(Routing(store) , document.querySelector("#root"));
+
