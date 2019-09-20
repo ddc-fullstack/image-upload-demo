974456f1877258953a02cd88e7e721eb53e2d1a9
george <gkephart@cnm.edu>
2019-09-20 11:53:06 -0600

    added fontawesome and added a profile page.

--- app/src/index.js
+++ app/src/index.js
@@ -5,11 +5,16 @@ import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {FourOhFour} from "./pages/FourOhFour";
 import {Home} from "./pages/Home";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {Profile} from "./pages/Profile";
+
+library.add();
 
 const Routing = () => (
 	<>
 		<BrowserRouter>
 			<Switch>
+				<Route exact path="/profile" component={Profile} />
 				<Route exact path="/" component={Home}/>
 				<Route component={FourOhFour}/>
 			</Switch>
---
+++ app/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Profile = () => {
+	return (
+
+		<h1>Y U NO FIND</h1>
+		</>
+	)
+};
