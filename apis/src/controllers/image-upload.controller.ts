1c0e4dc331e3eb7eeafbe4ef26bedcd527aa6272
gkephart <gkephart@cnm.edu>
2020-06-20 02:16:55 -0600

    so close yet so far 💩🥱👾

--- apis/src/controllers/image-upload.controller.ts
+++ apis/src/controllers/image-upload.controller.ts
@@ -1,9 +1,41 @@
 import {NextFunction, Request, Response} from "express";
-import {imageUploader} from "../lib/multer";
-import multer, {MulterError} from "multer";
+const DatauriParser = require('datauri/parser');
+import path from "path";
+const cloudinary = require('cloudinary').v2
+let streamifier = require('streamifier');
+
 
 export async function imageUploadController(request: Request, response: Response, nextFunction: NextFunction) {
 	try {
+		
+		cloudinary.config({
+			api_key: process.env.CLOUDINARY_KEY,
+			api_secret: process.env.CLOUDINARY_SECRET,
+			cloud_name: "cnm-ingenuity-deep-dive-bootcamp"
+		})
+		let uploadFromBuffer = (req: Request) => {
+			
+			return new Promise((resolve, reject) => {
+				
+				let cld_upload_stream = cloudinary.uploader.upload_stream(
+					(error: any, result: any) => {
+						
+						if (result) {
+							resolve(result);
+						} else {
+							reject(error);
+						}
+					}
+				);
+				
+				streamifier.createReadStream(req.file.buffer).pipe(cld_upload_stream);
+			});
+			
+		};
+		
+		const result = await uploadFromBuffer(request)
+		console.log(result)
+		
 		return response.json({status: 200, data: null, message: "this thing is on"})
 	} catch (error) {
 	console.log("error?",error.message)
--- apis/src/lib/multer.ts
+++ apis/src/lib/multer.ts
@@ -1,6 +1,10 @@
 import multer, {StorageEngine} from "multer";
 import {Express, Request} from "express";
 
+/*
+ * @see https://medium.com/@joeokpus/uploading-images-to-cloudinary-using-multer-and-expressjs-f0b9a4e14c54
+ */
+
 const storage : StorageEngine = multer.memoryStorage()
 const limits  = {fields: 2, files: 1, parts: 2 }
 const fileFilter : any = ( request: Request, file : Express.Multer.File , callback : any) => {
@@ -11,6 +15,8 @@ const fileFilter : any = ( request: Request, file : Express.Multer.File , callba
 }
 
 export const imageUploader = multer({storage, limits, fileFilter}).single('image')
+
+
 
 
 
