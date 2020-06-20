e6267e651a19d71a2a7494ee9e0b01de9f604d00
gkephart <gkephart@cnm.edu>
2020-06-20 00:40:27 -0600

    Multer is handling errors without crashing

--- apis/src/controllers/image-upload.controller.ts
+++ apis/src/controllers/image-upload.controller.ts
@@ -1,10 +1,14 @@
 import {NextFunction, Request, Response} from "express";
+import {imageUploader} from "../lib/multer";
+import multer, {MulterError} from "multer";
 
 export async function imageUploadController(request: Request, response: Response, nextFunction: NextFunction) {
 	try {
-		//request.file
 		return response.json({status: 200, data: null, message: "this thing is on"})
 	} catch (error) {
-		console.log(error)
+	console.log("error?",error.message)
 	}
 }
+
+
+
--- apis/src/lib/multer.ts
+++ apis/src/lib/multer.ts
@@ -1,4 +1,17 @@
 import multer, {StorageEngine} from "multer";
+import {Express, Request} from "express";
 
 const storage : StorageEngine = multer.memoryStorage()
-export const imageUploader = multer({storage}).single('image')
+const limits  = {fields: 2, files: 1, parts: 2 }
+const fileFilter : any = ( request: Request, file : Express.Multer.File , callback : any) => {
+	const {originalname} = file
+	return originalname.match(/\.(jpg|jpeg|png|gif)$/)
+		? callback(null, true)
+		: callback(new Error("only images are allowed to be uploaded"), false)
+}
+
+export const imageUploader = multer({storage, limits, fileFilter}).single('image')
+
+
+
+
